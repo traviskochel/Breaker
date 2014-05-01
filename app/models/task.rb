7a2016a254635233a43f2b998cb3b5aed57fb9c8
scribbletone <travis@scribbletone.com>
2014-05-01 09:29:14 -0700

    breadcrumb stylings

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,8 @@ class Task < ActiveRecord::Base
   belongs_to :deployment
   belongs_to :scenario
 
+  scope :completed, -> {where(completed: true).order('created_at DESC')}
+
+  scope :current, -> {where(completed: [false, nil]).order('created_at DESC')}
+
 end
